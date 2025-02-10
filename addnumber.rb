f15b9d37e8caf8b44f83b1c9333c037e242afddb
RunaDh17 <roonadangui@gmail.com>
2025-02-10 20:50:00 +0530

    Update addnumber.rb
    Negative number are taken care

--- addnumber.rb
+++ addnumber.rb
@@ -3,9 +3,12 @@
 #Output: an integer, sum of the numbers
 #multiple delimiter length greater than 1 char and different delimiter taken care
 #many numbers present in the string can be added
+#negative numbers are taken care
+#if negative numbers then no sum computed 
 
 num_string="1@#59#2" #ARGV[0]
 num_pos=[] # to store all positive numbers in the string
+num_neg=[] # to store all negative numbers in the string
 
 prev=""
 current=""
@@ -15,7 +18,9 @@ num_string.chars.each  do |c|
     current=c
     if current.match(/\d/)
       if prev.match(/\d/)     # multidigit number is taken care
-         num=num+current   
+         num=num+current  
+      elsif prev== "-"
+          num="-" + current #for multi digit negative number
       else
         num=current         #temporary digits holder
       end
@@ -23,13 +28,26 @@ num_string.chars.each  do |c|
       if num!="" and num.to_i > 0
          num_pos<< num.to_i
       end
+      if num!="" and num.to_i < 0
+         num_neg<< num.to_i
+      end
       num=""   #reset to blank when character is found
       
     end
     prev=current
 end
+#last number in the string is consider below
+if num!="" and num.to_i > 0
+         num_pos<< num.to_i
+end
+if num!="" and num.to_i < 0
+         num_neg<< num.to_i
+end
 
-puts num_pos.sum.to_s  #ruby >2.4
-
+if num_neg.empty?
+    puts  "Sum " + num_pos.sum.to_s  #ruby >2.4
+else
+    puts "negative numbers not allowed " + num_neg.join(',')
+end
 #puts num_pos.inject(0, :+).to_s  # ruby <2.4
 
